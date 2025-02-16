e39aee0943c8aba3bf4ece31bd8a9c94e016a074
marco-gulino <marcogulino98@gmail.com>
2025-02-16 19:10:39 +0100

    [feat] filled FooterCta with sign-up btn and socials cta

--- src/subcomponents/FooterCta.jsx
+++ src/subcomponents/FooterCta.jsx
@@ -1,4 +1,28 @@
 //FooterCta.jsx
 export default function FooterCta() {
-  return <div className="footer-cta"></div>;
+  return (
+    <div className="footer-cta">
+      <div className="signup-btn">SIGN-UP NOW!</div>
+      <div className="socials-menu">
+        <span>FOLLOW US</span>
+        <ul className="socials-cta">
+          <li>
+            <img src="./img/footer-facebook.png" alt="Facebook" />
+          </li>
+          <li>
+            <img src="./img/footer-twitter.png" alt="Twitter" />
+          </li>
+          <li>
+            <img src="./img/footer-youtube.png" alt="YouTube" />
+          </li>
+          <li>
+            <img src="./img/footer-pinterest.png" alt="Pinterest" />
+          </li>
+          <li>
+            <img src="./img/footer-periscope.png" alt="Periscope" />
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
