a2be68b25909793db839982a595ff4bdb458214c
marco-gulino <marcogulino98@gmail.com>
2025-02-16 18:43:59 +0100

    [feat] created "loadmore" button in ComicsList

--- src/subcomponents/ComicsList.jsx
+++ src/subcomponents/ComicsList.jsx
@@ -6,9 +6,12 @@ import ComicCard from "./ComicCard";
 //ComicsList.jsx
 export default function ComicsList(props) {
   return (
-    <div className="comicslist">
-      <div className="title-section">{props.sectionTitle}</div>
-      <ComicCard comicsData={comicsData} />
-    </div>
+    <>
+      <div className="comicslist">
+        <div className="title-section">{props.sectionTitle}</div>
+        <ComicCard comicsData={comicsData} />
+      </div>
+      <div className="loadmore-btn">LOAD MORE</div>
+    </>
   );
 }
