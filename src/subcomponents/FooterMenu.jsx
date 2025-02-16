1c2523729f743189ea34ffa3d2638e340668080c
marco-gulino <marcogulino98@gmail.com>
2025-02-16 20:05:29 +0100

    [feat] added menu in FooterMenu

--- src/subcomponents/FooterMenu.jsx
+++ src/subcomponents/FooterMenu.jsx
@@ -1,3 +1,51 @@
 export default function FooterMenu() {
-  return <div className="footer-menu"></div>;
+  return (
+    <div className="footer-menu-container">
+      <div className="footer-menu">
+        <div className="col">
+          <h2>DC COMICS</h2>
+          <ul>
+            <li>Characters</li>
+            <li>Comics</li>
+            <li>Movies</li>
+            <li>TV</li>
+            <li>Games</li>
+            <li>Videos</li>
+            <li>News</li>
+          </ul>
+          <h2>SHOP</h2>
+          <ul>
+            <li>Shop DC</li>
+            <li>Shop DC Collectibles</li>
+          </ul>
+        </div>
+        <div className="col">
+          <h2>DC</h2>
+          <ul>
+            <li>Terms Of Use</li>
+            <li>Privacy policy (New)</li>
+            <li>Ad Choices</li>
+            <li>Advertising</li>
+            <li>Jobs</li>
+            <li>Subscriptions</li>
+            <li>Talent Workshops</li>
+            <li>CPSC Certificates</li>
+            <li>Ratings</li>
+            <li>Shop Help</li>
+            <li>Contact Us</li>
+          </ul>
+        </div>
+        <div className="col">
+          <h2>SITES</h2>
+          <ul>
+            <li>DC</li>
+            <li>MAD Magazine</li>
+            <li>DC Kids</li>
+            <li>DC Universe</li>
+            <li>DC Power Visa</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
